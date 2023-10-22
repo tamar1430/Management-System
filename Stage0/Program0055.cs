d00ef6d52c3adbe21c410dd49f267fc195dba09d
michalk1924 <michalk1924@gmail.com>
2023-10-22 22:34:16 +0300

    Initial Code +Welcome functions

---
+++ Stage0/Program0055.cs
@@ -0,0 +1,16 @@
+﻿  internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome0055();
+    }
+
+    private static void Welcome0055()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}, welcome to my first cobsole application");
+    }
+
+    private static void Welocome8636() { }
+}
