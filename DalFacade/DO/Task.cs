c178efa3a9d6d0dc289490d8160370d0cd86c4b3
michalk1924 <michalk1924@gmail.com>
2023-11-04 20:34:14 +0200

    begin stage1

---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace DO;
+
+public record Task
+{
+    ///static int I = 1;
+    int Id;
+    string? Desceiption;
+    string? Alias;
+    bool? IsMilestone;
+
+
+
+
+}
