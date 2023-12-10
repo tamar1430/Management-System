3431085afd3d9354cd866b3dae7847b6ce207ab7
tamar1430 <tamarbr1430@gmail.com>
2023-12-10 23:16:10 +0200

    IBl

--- BL/BlApi/IMilestone.cs
+++ BL/BlApi/IMilestone.cs
@@ -1,5 +1,5 @@
 ﻿namespace BlApi;
-internal interface IMilestone
+public interface IMilestone
 {
     public BO.Engineer MilestoneDetailsRequest(int id);
     public void UpdateMilestoneData(BO.Milestone milestone);
