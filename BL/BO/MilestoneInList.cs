ea2ec4d99d68e820d67508c5139a3796950c532e
michalk1924 <michalk1924@gmail.com>
2023-12-10 21:59:28 +0200

    chapter 6 MilestoneInList

---
+++ BL/BO/MilestoneInList.cs
@@ -0,0 +1,11 @@
+﻿namespace BO;
+
+internal class MilestoneInList
+{
+    public int Id { get; init; }
+    public string Description {  get; init; }
+    public string Alias { get; init; }
+    public DateTime StartDate { get; init; }
+    public Status? Status { get; set; }
+    public double? CompletionPercentage { get; set; }
+}
