b61d5f5fd84c31856037d3c041378d4f69d13650
tamar1430 <tamarbr1430@gmail.com>
2023-12-10 21:59:52 +0200

    TaskInList

---
+++ BL/BO/TaskInList.cs
@@ -0,0 +1,10 @@
+﻿namespace BO
+{
+    public class TaskInList
+    {
+        public int Id { get; init; }
+        public string Description { get; init; }
+        public string Alias { get; init; }
+        public BO.Status Status { get; set; }
+    }
+}
