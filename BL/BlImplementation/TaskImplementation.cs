1ad0601759646d2521cca841e7cd6083bfbe42ae
tamar1430 <tamarbr1430@gmail.com>
2023-12-21 02:58:19 +0200

    contiune chapter 8

--- BL/BlImplementation/TaskImplementation.cs
+++ BL/BlImplementation/TaskImplementation.cs
@@ -13,16 +13,16 @@ internal class TaskImplementation : BlApi.ITask
         if (boTask.Id <= 0) throw new BO.BlInorrectData("Task's id isn't correct");
         if (boTask.Alias.Length <= 0) throw new BO.BlInorrectData("Task's Alias isn't correct");
 
-        foreach (var prevTask in boTask.Dependencies)
-        {
-            _dal.Dependency.Create(new DO.Dependency(0, boTask.Id, prevTask.Id));
-        }
+        //foreach (var prevTask in boTask.Dependencies)
+        //{
+        //    _dal.Dependency.Create(new DO.Dependency(0, boTask.Id, prevTask.Id));
+        //}
 
         DO.Task doTask = new(boTask.Id, boTask.Description,
             boTask.Alias, false, (DO.EngineerExperience)boTask!.CopmlexityLevel,
             boTask.CreatedAtDate,
             boTask.ScheduledStartDate, boTask.StartDate,
-            boTask.ForecastDate - boTask.BaselineStartDate, boTask.DeadLine,
+            boTask.ForecastDate - boTask.ScheduledStartDate, boTask.DeadLine,
             boTask.CompleteDate, boTask.Deliverable,
             boTask.Remarks, boTask!.Engineer!.Id
             );
@@ -41,7 +41,7 @@ internal class TaskImplementation : BlApi.ITask
     {
         if (_dal.Dependency.ReadAll().Where(d => d!.PreviousTask == id).First() != null)
         {
-            throw new BlDeletionImpossible("can't delete this task");
+            throw new BO.BlDeletionImpossible("can't delete this task");
         }
 
         try
@@ -62,30 +62,83 @@ internal class TaskImplementation : BlApi.ITask
                     Id = doTask.Id,
                     Description = doTask.Description,
                     Alias = doTask.Alias,
-                    CreatedAtDate = doTask.CreatedAtDate
-Status = null,
-                    Dependencies = doTask.IsMilestone ? new List<BO.Task>() :
-    Milestone
-     BaselineStartDate
-   StartDate
-    ScheduledStartDate
-   ForecastDate
-    DeadLine
-     CompleteDate
-    Deliverable
-   Remarks
-     Engineer
- CopmlexityLevel
+                    CreatedAtDate = doTask.CreatedAtDate,
+                    Status = doTask.ScheduledDate is null ? BO.Status.Unscheduled :
+                    doTask.StartDate is null ? BO.Status.Scheduled :
+                    doTask.CompleteDate is null ? BO.Status.OnTrack :
+                    BO.Status.InJeopardy,
+                    Milestone = null,//לעשות
+                                     //BaselineStartDate =null ,//לעשות
+                    StartDate = doTask.StartDate,
+                    ScheduledStartDate = doTask.ScheduledDate,
+                    ForecastDate = doTask.ScheduledDate + doTask.ForesastDate,
+                    DeadLine = doTask.DeadLineDate,
+                    CompleteDate = doTask.CompleteDate,
+                    Deliverable = doTask.Deliverable,
+                    Remarks = doTask.Remarks,
+                    Engineer = doTask.EngineerId is not null ? new BO.EngineerInTask() { Id = (int)doTask.EngineerId, Name = _dal!.Engineer!.Read((int)doTask!.EngineerId).Name } : null,
+                    CopmlexityLevel = (BO.EngineerExperience)doTask.CopmlexityLevel
                 });
     }
 
     public BO.Task Read(int id)
     {
-        throw new NotImplementedException();
+        DO.Task? doTask = _dal.Task.Read(id);
+        if (doTask is null)
+        {
+            throw new BO.BlDoesNotExistException($"Task with ID={id} doesn't exists");
+        }
+
+        BO.Task boTask = new()
+        {
+            Id = doTask.Id,
+            Description = doTask.Description,
+            Alias = doTask.Alias,
+            CreatedAtDate = doTask.CreatedAtDate,
+            Status = doTask.ScheduledDate is null ? BO.Status.Unscheduled :
+                doTask.StartDate is null ? BO.Status.Scheduled :
+                doTask.CompleteDate is null ? BO.Status.OnTrack :
+                BO.Status.InJeopardy,
+            Milestone = null,//לעשות
+                             //BaselineStartDate =null ,//לעשות
+            StartDate = doTask.StartDate,
+            ScheduledStartDate = doTask.ScheduledDate,
+            ForecastDate = doTask.ScheduledDate + doTask.ForesastDate,
+            DeadLine = doTask.DeadLineDate,
+            CompleteDate = doTask.CompleteDate,
+            Deliverable = doTask.Deliverable,
+            Remarks = doTask.Remarks,
+            Engineer = doTask.EngineerId is not null ? new BO.EngineerInTask() { Id = (int)doTask.EngineerId, Name = _dal!.Engineer!.Read((int)doTask!.EngineerId).Name } : null,
+            CopmlexityLevel = (BO.EngineerExperience)doTask.CopmlexityLevel
+        };
+        return boTask;
     }
 
-    public void Update(BO.Task engineer)
+    public void Update(BO.Task boTask)
     {
-        throw new NotImplementedException();
+        if (boTask.Id <= 0) throw new BO.BlInorrectData("Task's id isn't correct");
+        if (boTask.Alias.Length <= 0) throw new BO.BlInorrectData("Task's Alias isn't correct");
+
+        //foreach (var prevTask in boTask.Dependencies)
+        //{
+        //    _dal.Dependency.Create(new DO.Dependency(0, boTask.Id, prevTask.Id));
+        //}
+
+        DO.Task? prevDoTask = _dal!.Task.Read(boTask.Id);
+
+        if (prevDoTask is null) throw new BO.BlDoesNotExistException($"task with id:{boTask.Id} isn't exists");
+
+        _dal.Task.Delete(boTask.Id);
+
+        DO.Task newDoTask = new(boTask.Id, boTask.Description,
+            boTask.Alias, false, (DO.EngineerExperience)boTask!.CopmlexityLevel,
+            boTask.CreatedAtDate,
+            boTask.ScheduledStartDate, boTask.StartDate,
+            boTask.ForecastDate - boTask.ScheduledStartDate, boTask.DeadLine,
+            boTask.CompleteDate, boTask.Deliverable,
+            boTask.Remarks, boTask!.Engineer!.Id
+            );
+        _dal.Task.Create(newDoTask);
     }
 }
+
